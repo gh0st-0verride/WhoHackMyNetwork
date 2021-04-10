2d87980a98336c684b3d65219de052f198500449
gh0st-0verride <79782581+gh0st-0verride@users.noreply.github.com>
2021-04-10 09:34:06 +0300

    Add files via upload

---
+++ WhoHackMyNetwork/Codes/main.py
@@ -0,0 +1,20 @@
+from os import popen as system
+from sys import argv
+def get_var():
+    out = system("Execute/scan.exe " + argv[1])
+    global ips
+    ips = ((out.read()).split("\n"))
+def edit():
+    global ips
+    for i in range(0,4):
+        ips.pop(0)
+    for i in range(0,len(ips)):
+        ips[i] = ips[i].split()
+    ips.pop(len(ips)-1)
+def main():
+    get_var()
+    edit()
+    global ips
+    for i in ips:
+        print(i[0] + " " + i[1] + " " + i[2])
+main()
